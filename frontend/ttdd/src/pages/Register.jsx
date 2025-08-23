43356ebb9b8af979f88dede2986984365671913e
Prashant Batchu <prasanthbatchu6@gmail.com>
2025-08-23 12:18:05 +0530

    added passw

--- frontend/ttdd/src/pages/Register.jsx
+++ frontend/ttdd/src/pages/Register.jsx
@@ -80,7 +80,7 @@ const Register = () => {
             </div>
             <div>
               <label htmlFor="password" className="sr-only">
-                Password with min 6-characters
+                Password 
               </label>
               <input
                 id="password"
@@ -89,7 +89,7 @@ const Register = () => {
                 autoComplete="new-password"
                 required
                 className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 focus:outline-none focus:ring-blue-500 focus:border-blue-500 focus:z-10 sm:text-sm"
-                placeholder="Password"
+                placeholder="Password with minimum 6 characters"
                 value={password}
                 onChange={(e) => setPassword(e.target.value)}
               />
